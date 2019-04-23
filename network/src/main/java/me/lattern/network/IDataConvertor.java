ba5fd4da4f2fb545818ee0a161fddc1e8c56054d
Huang Peng <huangpeng@chunyu.me>
2019-04-23 16:51:43 +0800

    MOD: remove network

--- network/src/main/java/me/lattern/network/IDataConvertor.java
+++
@@ -1,5 +0,0 @@
-package me.lattern.network;
-
-public abstract class IDataConvertor {
-    public abstract <T> T convert(Object data, Class<T> clazz);
-}
--- network/src/main/java/me/lattern/network/JsonDataConvertor.java
+++
@@ -1,8 +0,0 @@
-package me.lattern.network;
-
-public class JsonDataConvertor extends IDataConvertor {
-    @Override
-    public <T> T convert(Object data, Class<T> clazz) {
-        return null;
-    }
-}
